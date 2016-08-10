e1ad8dd2e37ae7e5254c767fcb3fd100dc250e5a
Yilin Peng <elenayyyp@gmail.com>
2016-08-09 21:28:54 -0400

    hii

---
+++ src/GoogleMap.js
@@ -0,0 +1,22 @@
+// import React, { Component } from 'react';
+// import LoadJS from './Load.js';
+// import './Map.js';
+// class GoogleMap extends Component {
+//
+//
+//   componentWillMount(){
+//
+//     loadJS("https://maps.googleapis.com/maps/api/js?key=AIzaSyBI7-TOCmscy28RdbmUuLBf7yYkz_kw_H4&libraries=places&callback=initMap async defer");
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <div id="map">
+//
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default GoogleMap
